501bd6ec527b3b2910242130bdb0323403c6edac
Jack Christensen <jack@jackchristensen.com>
2020-04-02 21:36:25 -0500

    pgx-707

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/jackc/pgtype"
 	"github.com/jackc/pgx/v4"
 )
 
@@ -19,4 +21,18 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer conn.Close(context.Background())
+
+	_, err = conn.Exec(context.Background(), `drop type if exists custom; create type custom as (a int, b text);`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	genericBinary := &pgtype.GenericBinary{}
+	err = conn.QueryRow(context.Background(), `select '(42,foo)'::custom`, pgx.QueryResultFormats{pgx.BinaryFormatCode}).Scan(genericBinary)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(genericBinary.Bytes)
+
 }
